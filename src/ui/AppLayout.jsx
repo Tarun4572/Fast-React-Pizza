e2f58e13e61230f110c74341db4171f4e5b2d483
pottigari.tarun <pottigaritarun01@gmail.com>
2024-03-27 21:36:37 +0530

    tailwind implementation

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+import CartOveriew from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOveriew />
+    </div>
+  );
+}
+
+export default AppLayout;
